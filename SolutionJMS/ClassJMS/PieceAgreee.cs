9ba89f430d7997fb672ef1d72a3b6a9eddde1572
fuzionzeak <fuzionzeak@gmail.com>
2023-09-14 17:37:48 +0200

    travail jusqu'à 2.C

---
+++ SolutionJMS/ClassJMS/PieceAgreee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassJMS
+{
+    public class PieceAgreee : Piece
+    {
+        private DateTime dateAgrement;
+        private string nomConstructeur;
+
+        public PieceAgreee(int numSerie, string libelle, int nbHeures, DateTime dateAgrement, string nomConstructeur)
+            : base(numSerie, libelle, nbHeures)
+        {
+            this.dateAgrement = dateAgrement;
+            this.nomConstructeur = nomConstructeur;
+        }
+
+        public void renouvelerAgrement(DateTime nouvelleDate)
+        {
+            this.dateAgrement = nouvelleDate;
+        }
+        /*
+        public int CalculerDureeAgrement()
+        {
+         TODO
+        }
+        */
+
+
+    }
+}
